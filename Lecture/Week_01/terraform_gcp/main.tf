126b53d8d09ad6c70ae0ed58089459804c868e35
Prabin <Prbn@users.noreply.github.com>
2025-03-10 01:37:22 -0400

    Update

--- Lecture/Week_01/terraform_gcp/main.tf
+++ Lecture/Week_01/terraform_gcp/main.tf
@@ -36,3 +36,60 @@ resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.location
 }
+
+
+resource "google_compute_instance" "dezc-1" {
+  boot_disk {
+    auto_delete = true
+    device_name = var.gci_name
+
+    initialize_params {
+      image = var.gci_image
+      size  = var.gci_disk_size
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = var.gci_machine_type
+  name         = var.gci_name
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork = "projects/${var.project}/regions/${var.location}/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = var.gcp_service_acc_email
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = var.gci_zone
+}
--- Lecture/Week_01/terraform_gcp/variables.tf
+++ Lecture/Week_01/terraform_gcp/variables.tf
@@ -1,7 +1,7 @@
 
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "/Users/prbnrs/.keys/.gc/ny-rides-prbn.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -9,34 +9,76 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "ny-rides-prbn"
 }
 
 variable "region" {
   description = "Region"
   #Update the below to your desired region
-  default     = "us-central1"
+  default = "us-central1"
 }
 
 variable "location" {
   description = "Project Location"
   #Update the below to your desired location
-  default     = "US"
+  default = "US"
 }
 
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "demo_dataset"
+  default = "demo_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default = "terraform-demo-terra-bucket"
 }
 
 variable "gcs_storage_class" {
   description = "Bucket Storage Class"
   default     = "STANDARD"
 }
+
+variable "gcp_service_acc_name" {
+  description = "GCP Service Account Name"
+  default     = "STANDARD"
+}
+
+variable "gcp_service_acc_email" {
+  description = "GCP Service Account Email"
+  default     = "terraform-runner@ny-rides-prbn.iam.gserviceaccount.com"
+}
+
+variable "gci_name" {
+  description = "Google Compute Instance Name"
+  #Update the below to what you want your Compute Instance to be called
+  default = "dezc-1"
+}
+
+variable "gci_image" {
+  description = "Google Compute Instance Image"
+  #Update the below to what Compute Instance Image you want
+  default = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20250213"
+}
+
+variable "gci_disk_size" {
+  description = "Google Compute Instance disk Size in GB"
+  #Update the below to what Compute Instance disk Size you want
+  default = 30
+}
+
+variable "gci_machine_type" {
+  description = "Google Compute Instance Machine Type"
+  #Update the below to what Compute Instance Machine Type you want
+  default = "e2-standard-4"
+}
+
+variable "gci_zone" {
+  description = "Google Compute Instance Zone"
+  #Update the below to what Compute Instance Machine Type you want
+  default = "us-central1-c"
+}
+
+
